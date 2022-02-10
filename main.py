9f8c40f870fbdf51c20714fcc1ef9a6901b4690f
Fravel <fravelhm@appstate.edu>
2022-02-10 15:37:08 -0500

    First commit! Learning tkinter

---
+++ main.py
@@ -0,0 +1,24 @@
+import Tkinter as tk
+
+window = tk.Tk()
+
+greeting = tk.Label(
+    text="SUP",
+    fg='pink', bg='black',
+    width=25, height=2)
+greeting.pack(fill=tk.X)
+
+entry = tk.Entry(
+    fg="green", bg="blue",
+    width=25
+)
+entry.pack(fill=tk.X)
+
+button = tk.Button(
+    text="Other SUP",
+    width=25, height=2,
+    fg="purple", bg="yellow"
+)
+button.pack(fill=tk.X)
+
+window.mainloop()
