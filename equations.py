c87fc94b6c347edbfcb0c1f8bbc47aee42ffc80c
hfravel <hmfravel@gmail.com>
2022-03-17 12:11:04 -0400

    Added more equations

--- equations.py
+++ equations.py
@@ -1,3 +1,5 @@
+from inspect import signature
+
 class AstronomyEquations:
     # This is a class that holds all my equations and
     # all of the methods to print the equations
@@ -6,6 +8,9 @@ class AstronomyEquations:
         #                 0        1        2        3        4        5        6        7        8        9
         self.supNums = ['\u2070','\u00b9','\u00b2','\u00b3','\u2074','\u2075','\u2076','\u2077','\u2078','\u2079']
         self.subNums = ['\u2080','\u2081','\u2082','\u2083','\u2084','\u2085','\u2086','\u2087','\u2088','\u2089']
+
+        # Greek Characters
+        self.greek = {'a': '\u03B1', 'o': '\u03B8'}
         # End init
 
     # Returns a list of all the equation methods
@@ -52,6 +57,22 @@ class AstronomyEquations:
     def motion4Print(self):
         return "Motion -- v%s = 0.5(v%s + v%s)" % ('\u0304', self.subNums[1], self.subNums[0])
 
+    # Astronomy Equations
+    def astDistance1Equation(self, alpha, d):
+        return alpha * d / 206265
+    def astDistance1Print(self):
+        return 'D = %sd / 206265' % (self.greek['a'])
+    def astDistance2Equation(self, alpha, D):
+        return 206265 * D / alpha
+    def astDistance2Print(self):
+        return 'd = 206265D / %s' % (self.greek['a'])
+    def astDistance3Equation(self, theta):
+        return 1 / theta
+    def astDistance4Print(self):
+        return 'd = 1 / %s' % (self.greek['o'])
+
+
+
 if (__name__ == '__main__'):
     ae = AstronomyEquations()
     print("EQUATIONS")
@@ -60,3 +81,5 @@ if (__name__ == '__main__'):
     #pm = ae.getAllPrints()
     for eq in ae.getAllPrints():
         print(getattr(ae, eq)())
+
+    #print(len(signature(ae.motion4Equation).parameters))
